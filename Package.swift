d2e7a331c4c6d597b580842267e0a0e15774b555
Bailey Nahi <bailey@that.global>
2025-11-14 02:53:33 +1000

    pin secp256k1 (used by web3swift & solana-swift)

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
     ],
     dependencies: [
         // Main depedencies
-        .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift", .upToNextMinor(from: "0.10.0")),
+        // pin secp256k1 (used by web3swift & solana-swift)
+        .package(url: "https://github.com/21-DOT-DEV/swift-secp256k1.git", exact: "0.10.0"),
         // .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
         .package(url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap.git", from: "1.0.2"),
         .package(url: "https://github.com/bigearsenal/task-retrying-swift.git", from: "2.0.0"),
@@ -30,8 +31,7 @@ let package = Package(
         .target(
             name: "SolanaSwift",
             dependencies: [
-                "secp256k1",
-                // .product(name: "secp256k1", package: "secp256k1.swift"),
+                .product(name: "secp256k1", package: "swift-secp256k1"),
                 .product(name: "TweetNacl", package: "tweetnacl-swiftwrap"),
                 .product(name: "Task_retrying", package: "task-retrying-swift"),
             ]
